716a8d7e7e78ca179d3ce20eb394cd98047e2371
Santosh Bala <santmartian@gmail.com>
2023-10-24 16:31:44 +0530

    use spotify API

--- functions.py
+++ functions.py
@@ -3,11 +3,16 @@ import yt_dlp as youtube_dl
 from pprint import PrettyPrinter
 import requests
 import os
-
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import time
 #pp=PrettyPrinter()
 
+client_credentials_manager = SpotifyClientCredentials(client_id="e75a7570a4a64aa9bb953f2c05f7f136", client_secret="fac7299b84854f459874bcb149c7df40")
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
 ydl_opts = {
-    'outtmpl': "Audio" + '/%(title)s.%(ext)s',
+    'outtmpl': "new",
     'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
@@ -18,16 +23,22 @@ ydl_opts = {
 
 def search(stringy):
     """
-    Accepts a string for searching and returns a dictionary of url,duration,prettyname and thumbnail url
+    Accepts a string and uses youtube and spotify API to get metadata.
+    Returns a dictionary of url, duration, prettyname and thumbnail url.
     """
     videosSearch = VideosSearch(stringy, limit = 1)
     Videos=videosSearch.result()
 #    pp.pprint(Videos)
     url="https://www.youtube.com/watch?v="+Videos["result"][0]["id"]
-    duration=Videos["result"][0]["duration"]
-    pretty_name=Videos["result"][0]["title"]
-    thumbnail_url=Videos["result"][0]["thumbnails"][0]["url"]
-    return {"url":url,"duration":duration,"pretty_name":pretty_name,"thumbnail_url":thumbnail_url}
+    details=sp.search(q=stringy,limit=1,type="track")
+    duration=details["tracks"]["items"][0]["duration_ms"]//1000 #returns in ms
+    pretty_name=details["tracks"]["items"][0]["name"]
+    thumbnail_url=details["tracks"]["items"][0]["album"]["images"][0]["url"]
+    L_artists=details["tracks"]["items"][0]["artists"]
+    artists=[]
+    for i in L_artists:
+         artists.append(i["name"])
+    return {"url":url,"duration":duration,"pretty_name":pretty_name,"thumbnail_url":thumbnail_url,"artists":artists}
 #print(search("ordinary person"))
 
 def thumbs(name,url):
@@ -36,23 +47,43 @@ def thumbs(name,url):
     """
     res=requests.get(url).content
 #    print("res",res,"url",url)
-    file=open(name+".png","wb")
+    file=open(os.path.join("thumbs/",f"{name}.png"),"wb")
     file.write(res)
     file.close()
 
 #thumbs("blindinglights",search("blinding lights")["thumbnail_url"])
+# def better_name(stringy):
+#     stringy=stringy.split()
+#     new_stringy=""
+#     for i in stringy:
+#         new_stringy+=i
+#     new_stringy=new_stringy.casefold()
+#     return new_stringy
 
-def download(url,pretty_name,name):
+def better_name(stringy):
+    # Remove spaces and other special characters from the string
+    new_stringy = ''.join(char for char in stringy if char.isalnum() or char in ['_', '-', '.'])
+    new_stringy = new_stringy.casefold()
+    return new_stringy
+  
+def download(url,name):
     """
     Function to download song and convert it to mp3.
     Accepts url, Actual song name (pretty_name) and filename
     """
+    name=name+r".mp3"
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
           ydl.download([url])
-          info = ydl.extract_info(url, download=False)
-          info_with_audio_extension = dict(info)
-          info_with_audio_extension['ext'] = "mp3"
-          x=ydl.prepare_filename(info_with_audio_extension)
-          os.rename(x,os.path.join("Audio/",name))
-#t=search("ordinary person")
-#download(t["url"],t["pretty_name"],"ordinaryperson.mp3")
+          try:
+              os.rename("new.mp3",os.path.join(r"Audio/",name))
+          except FileExistsError:
+              print("already cached")
+              try:
+                  os.remove("new.mp3")
+              except:
+                  print("this shldnt happen")       
+              
+t=search("badass from leo")
+download(t["url"],better_name(t["pretty_name"]))
+thumbs(better_name(t["pretty_name"]),t["thumbnail_url"])
+print(t)
