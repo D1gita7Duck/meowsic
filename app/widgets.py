bf9164746940df65fe58680dfa5603992d4eb204
D1gita7 Duck <arnavp3007@gmail.com>
2023-11-30 15:29:17 +0530

    switching tabs deletes any and all playlist tabs

--- app/widgets.py
+++ app/widgets.py
@@ -171,6 +171,31 @@ def return_pressed(event=None):
     print(event)
     print('RETURN PRESSED')
 
+def on_mouse_click(_event=None):
+    '''
+    Focusses the widget that is clicked (mouse1). 
+    Deletes all playlist tabs (if open)
+    '''
+    _event.widget.focus_set()
+
+    current_focus=(str(app.focus_get()).split('.'))
+
+    # print(master_tab._tab_dict)
+
+    # check if currently focussed widget is not a playlist tab
+    if current_focus[-3]=='!ctksegmentedbutton' and current_focus[-2] in ['!ctkbutton5','!ctkbutton4','!ctkbutton3','!ctkbutton2','!ctkbutton']:
+        try:
+            # try to delete the playlist tab
+            for name in master_tab._tab_dict:
+                if name not in ['Home', 'Queue', 'Search', 'Your Library', 'Liked Songs']:
+                    master_tab._name_list.remove(name)
+                    master_tab._tab_dict[name].grid_forget()
+                    master_tab._tab_dict.pop(name)
+                    master_tab._segmented_button.delete(name)
+        except RuntimeError: 
+            # runtime error is flashed as the master_tab._tab_dict changes size while the function is called
+            pass
+
 # menu
 menu = CTkMenuBar.CTkMenuBar(app)
 menu.lift()
@@ -709,8 +734,9 @@ status_bar.configure(state='disabled')
 
 '''Key Bindings'''
 
-# bind buttons
-app.bind("<1>", lambda event: event.widget.focus_set())
+# bind mouse1
+# <1> is a synonym for <ButtonPress-1> (aka left mouse button)
+app.bind("<1>", lambda event: on_mouse_click(_event=event))
 
 # bind space key to play/pause
 app.bind('<space>', lambda event: music.play_pause(play_button, _event=event) if type(app.focus_get())!=tkinter.Entry else print('Focus in EntryBox'))
